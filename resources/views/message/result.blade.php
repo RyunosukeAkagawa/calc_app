0aae369ccb96329d43e63839406a6a6477323a85
RyunosukeAkagawa <ryunosuke19910601@gmail.com>
2023-10-23 08:44:21 +0900

    計算アプリの課題でエラーが表示されたので確認お願いします

---
+++ resources/views/message/result.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>計算アプリの課題</title>
+</head>
+<body>
+    <h1>計算アプリ</h1>
+    <p>{{ $num1 }},{{ $num2 }},{{ $calc }}</p>
+</body>
+</html>
